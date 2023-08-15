eacbb93f91809f4c52c9e6be7694107a3122bfac
roland444 <123905024+roland444@users.noreply.github.com>
2023-08-15 22:14:20 +0200

    Added showQuote function

--- app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-import data from './quotes.json' assert {type: 'json'};
-
-console.log(data);
--- index.js
+++ index.js
@@ -8,9 +8,16 @@ window.onload = () => {
 async function getData() {
     const response = await fetch("./quotes.json");
     const data = await response.json();
-    console.log(data[0].quote);
+    showQuote(data);
 }
 
-function loadQuote(data) {
-
+function showQuote(data) {
+    data = data[(Math.floor(randomIndex(1, 109))) - 1];
+    _quote.innerHTML += data.quote;
+    _author.innerHTML += data.author;
 }
+
+function randomIndex(mn, mx) {
+    return Math.random() * (mx - mn) + mn;
+}
+
